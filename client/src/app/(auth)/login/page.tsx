2ffb4801e0c2f01b68d85f087b7a0e7820f8ed22
maruf-pfc <mdmarufsarker.mms@gmail.com>
2025-09-09 03:50:24 +0600

    chore: setup better auth, neon and oauth

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -6,13 +8,53 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { GithubIcon } from "lucide-react";
+import { GithubIcon, Loader } from "lucide-react";
 import { FcGoogle } from "react-icons/fc";
-import React from "react";
+import React, { useTransition } from "react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { signIn } from "@/lib/auth-client";
+import { toast } from "sonner";
 
 const LoginPage = () => {
+  const [googlePending, startGoogleTransition] = useTransition();
+  const [githubPending, startGithubTransition] = useTransition();
+
+  async function signInWithGoogle() {
+    startGoogleTransition(async () => {
+      await signIn.social({
+        provider: "google",
+        callbackURL: "/",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Sign in with Google successful"),
+              toast.info("Redirecting to the home page");
+          },
+          onError: () => {
+            toast.error("Internal Server Error");
+          },
+        },
+      });
+    });
+  }
+  async function signInWithGitHub() {
+    startGithubTransition(async () => {
+      await signIn.social({
+        provider: "github",
+        callbackURL: "/",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Sign in with GitHub successful"),
+              toast.info("Redirecting to the home page");
+          },
+          onError: () => {
+            toast.error("Internal Server Error");
+          },
+        },
+      });
+    });
+  }
+
   return (
     <Card>
       <CardHeader>
@@ -23,13 +65,41 @@ const LoginPage = () => {
       </CardHeader>
 
       <CardContent className="flex flex-col gap-4">
-        <Button className="w-full cursor-pointer" variant={"outline"}>
-          <FcGoogle className="size-4" />
-          <span>Sign in with Google</span>
+        <Button
+          className="w-full cursor-pointer"
+          variant={"outline"}
+          onClick={signInWithGoogle}
+          disabled={googlePending}
+        >
+          {googlePending ? (
+            <>
+              <Loader className="size-4 animate-spin" />
+              <span>Loading...</span>
+            </>
+          ) : (
+            <>
+              <FcGoogle className="size-4" />
+              <span>Sign in with Google</span>
+            </>
+          )}
         </Button>
-        <Button className="w-full cursor-pointer" variant={"outline"}>
-          <GithubIcon className="size-4" />
-          <span>Sign in with GitHub</span>
+        <Button
+          className="w-full cursor-pointer"
+          variant={"outline"}
+          onClick={signInWithGitHub}
+          disabled={githubPending}
+        >
+          {githubPending ? (
+            <>
+              <Loader className="size-4 animate-spin" />
+              <span>Loading...</span>
+            </>
+          ) : (
+            <>
+              <GithubIcon className="size-4" />
+              <span>Sign in with GitHub</span>
+            </>
+          )}
         </Button>
 
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
