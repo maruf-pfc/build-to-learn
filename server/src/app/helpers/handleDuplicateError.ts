a85aa86d730c9a5a811169c03e9309eb5b5eec9d
maruf-pfc <mdmarufsarker.mms@gmail.com>
2025-11-08 22:00:47 +0600

    add: quiz module and fix file formats of the server

--- server/src/app/helpers/handleDuplicateError.ts
+++ server/src/app/helpers/handleDuplicateError.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { IGenericErrorResponse } from "../interfaces/error.types";
 
-export const handleDuplicateError = (err: any) : IGenericErrorResponse => {
+export const handleDuplicateError = (err: any): IGenericErrorResponse => {
   const matchedArray = err.message.match(/"([^"]*)"/);
 
   return {
--- server/src/app/middlewares/checkEnrollment.ts
+++ server/src/app/middlewares/checkEnrollment.ts
@@ -4,12 +4,24 @@ import httpStatus from "http-status-codes";
 import { JwtPayload } from "jsonwebtoken";
 import { Enrollment } from "../modules/enrollment/enrollment.model";
 
-export const checkEnrollment = async (req: Request, _res: Response, next: NextFunction) => {
+export const checkEnrollment = async (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
   const token = req.user as JwtPayload;
   const { courseId } = req.params;
 
-  const doc = await Enrollment.findOne({ course: courseId, user: token.userId, isDeleted: false });
-  if (!doc) throw new AppError(httpStatus.FORBIDDEN, "Please enroll (or complete payment) to access this content");
+  const doc = await Enrollment.findOne({
+    course: courseId,
+    user: token.userId,
+    isDeleted: false,
+  });
+  if (!doc)
+    throw new AppError(
+      httpStatus.FORBIDDEN,
+      "Please enroll (or complete payment) to access this content"
+    );
 
   return next();
 };
